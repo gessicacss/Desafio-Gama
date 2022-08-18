2ea56841bb4d1c06b542a8fef1a33aa428a5077d
Géssica Santos <111399040+gessicacss@users.noreply.github.com>
2022-08-18 19:46:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var numero = [];
+var media;
+numero[1] = prompt('Primeiro Número', '');
+numero[1] = eval (numero[1])
+numero[2] = prompt('Segundo Numero', '');
+numero[2] = eval (numero[2])
+numero[3] = prompt('Terceiro Número', '');
+numero[3] = eval (numero[3])
+
+media = (numero[1] + numero[2] + numero[3])/3;
+
+console.log('a média é' + media)
